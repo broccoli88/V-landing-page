b157b647937c929a0db1cde1d2c5e926a7dff3ac
broccoli88 <kode.kudaki@gmail.com>
2022-04-09 19:36:09 +0200

    Added navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,26 @@
 <template>
-    <div class="nav-bar-mobile">
+    <div class="nav-container">
         <span class="logo">Traveler</span>
-        <div class="burger" @click="toggleNav">
+        <div class="burger" @click="toggleNav" v-if="showBurger">
             <div class="line" :class="{ line1: showNavMobile }"></div>
             <div class="line" :class="{ line2: showNavMobile }"></div>
             <div class="line" :class="{ line3: showNavMobile }"></div>
         </div>
+
         <nav>
-            <ul class="nav-links-mobile" v-show="showNavMobile">
+            <transition @enter="onEnter" name="nav-slide">
+                <ul class="nav-links-mobile" v-if="showNavMobile">
+                    <li>Offer</li>
+                    <li>Destinations</li>
+                    <li>Prices</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </transition>
+        </nav>
+
+        <nav v-if="showNavBar" class="standard-nav">
+            <ul class="nav-links">
                 <li>Offer</li>
                 <li>Destinations</li>
                 <li>Prices</li>
@@ -19,29 +32,60 @@
 </template>
 
 <script>
+import gsap from "gsap";
 export default {
     data() {
         return {
-            showNavMobile: false,
+            showNavMobile: null,
+            showNavBar: null,
+            showBurger: null,
         };
+    },
+
+    mounted() {
+        window.addEventListener("resize", this.checkWidth);
+        this.checkWidth();
     },
 
     methods: {
         toggleNav() {
-            const links = document.querySelectorAll(".nav-links-mobile li");
             this.showNavMobile = !this.showNavMobile;
-            console.log(links);
+        },
+
+        onEnter(el) {
+            const links = el.children;
+            gsap.to(links, {
+                x: 0,
+                opacity: 1,
+                stagger: {
+                    amount: 0.2,
+                },
+            });
+        },
+
+        checkWidth() {
+            const check = window.innerWidth;
+
+            if (check >= 650) {
+                this.showBurger = false;
+                this.showNavMobile = false;
+                this.showNavBar = true;
+            } else {
+                this.showBurger = true;
+                this.showNavBar = false;
+            }
         },
     },
 };
 </script>
 
 <style>
-.nav-bar-mobile {
+.nav-container {
     height: 7rem;
 
     display: flex;
     align-items: center;
+    justify-content: space-between;
 
     box-shadow: 0 0 10px hsl(0, 0%, 0%, 0.5);
 
@@ -104,8 +148,13 @@ export default {
 
 .nav-links-mobile > li {
     width: 100%;
+    color: var(--color-secondary);
 
     position: relative;
+
+    transform: translateX(420px);
+    opacity: 0;
+
     transition: all 0.3s ease;
 }
 
@@ -114,8 +163,8 @@ export default {
     display: inline-block;
     width: 100%;
     height: 2px;
-    background-color: hsl(0, 0%, 0%, 0.5);
-    box-shadow: 0 2px 2px hsl(0, 0%, 0%, 0.5);
+    background-color: hsla(0, 15%, 52%, 0.5);
+    box-shadow: 0 2px 2px hsla(0, 15%, 52%, 0.5);
 
     position: absolute;
     bottom: 0;
@@ -146,5 +195,50 @@ export default {
 .line3 {
     transform: translateY(10px);
     opacity: 0;
+}
+
+.nav-slide-enter-from,
+.nav-slide-leave-to {
+    transform: translateX(250px);
+    opacity: 0;
+}
+
+.nav-slide-enter-active,
+.nav-slide-leave-active {
+    transition: all 0.5s ease;
+}
+
+.standard-nav {
+    width: 95%;
+
+    position: fixed;
+    /* top: 0;
+    left: 0; */
+}
+
+.nav-links {
+    width: 100%;
+    list-style: none;
+    font-family: var(--font-lobster);
+
+    display: flex;
+    justify-content: flex-end;
+    gap: 0.3em;
+}
+
+.nav-links > li {
+    cursor: pointer;
+    width: 6em;
+    text-align: center;
+    color: var(--color-secondary);
+
+    position: relative;
+}
+
+.nav-links > li:hover {
+    font-weight: 700;
+}
+
+@media (min-width: 650px) {
 }
 </style>
